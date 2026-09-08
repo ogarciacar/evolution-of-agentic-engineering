a6ba94d4d3b0bc2891fd367de3deb031ba388563
Orlando <orlando.c@miro.com>
2026-09-08 19:24:33 +0200

    S2: test evidence-scoped practice observations

--- pipeline/test_practice_observations.py
+++ pipeline/test_practice_observations.py
@@ -9,6 +9,7 @@ class PracticeObservationTest(unittest.TestCase):
         values = {
             "id": "spotify-lineage-01",
             "projection_id": "main",
+            "evidence_id": "2025-11-06-spotify-honk-part-1",
             "use_case": "Find affected repositories",
             "problem": "The agent must discover downstream consumers",
             "reported_practice": "Use dependency lineage",
@@ -37,7 +38,7 @@ class PracticeObservationTest(unittest.TestCase):
             self.observation(selection_conditions=())
 
     def test_rejects_required_empty_strings(self):
-        for field in ("id", "projection_id", "use_case", "problem", "reported_practice"):
+        for field in ("id", "projection_id", "evidence_id", "use_case", "problem", "reported_practice"):
             with self.subTest(field=field), self.assertRaisesRegex(ValueError, field):
                 self.observation(**{field: "   "})
 
@@ -45,6 +46,11 @@ class PracticeObservationTest(unittest.TestCase):
         main = self.observation(projection_id="main")
         preview = self.observation(projection_id="0123456789ab")
         self.assertNotEqual(main, preview)
+
+    def test_identical_observations_from_different_evidence_remain_distinct(self):
+        first = self.observation(evidence_id="2025-11-06-spotify-honk-part-1")
+        second = self.observation(evidence_id="2025-11-24-spotify-honk-part-2")
+        self.assertNotEqual(first, second)
 
     def test_rejects_duplicate_conditions(self):
         with self.assertRaisesRegex(ValueError, "duplicates"):
